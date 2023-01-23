22b4e4dfdb12bbd97161e21ffa27f46bdfc4ae2f
hooman mousavi <hoomanmousavi77@gmail.com>
2023-01-24 01:52:53 +0330

    initialize project

---
+++ dist/main.5b56132777e33641383a.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatetemplate"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("9cb285c6824a926403e3")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.5b56132777e33641383a.hot-update.js.map
---
+++ webpack.prod.config.ts
@@ -0,0 +1,71 @@
+const path=require('path')
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    devtool:'source-map',
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname,"./dist"),
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/[name][ext]"
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g)/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)/,
+                type: 'asset/inline'
+            },
+            {
+                test: /\.(ts|js)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-typescript"],
+                    },
+                },
+            },
+            {
+                test: /\.ts$/,
+                exclude: [/node_modules/],
+                loader: 'ts-loader'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:"css/[name].css"
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "index.html"
+            // publicPath:"./dist/"
+        })
+    ]
+}
+
+export {}
